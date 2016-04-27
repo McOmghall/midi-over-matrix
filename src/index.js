0ee33719b56988bd745880498279737981454866
McOmghall <pmgberm@gmail.com>
2016-04-27 15:57:58 +0200

    Added capabilities to activate and deactivate jams

--- src/index.js
+++ src/index.js
@@ -10,19 +10,22 @@ var midiOverMatrixCtrl = ['MatrixManager', 'MidiManager', '$scope', '$log', '$in
   $scope.ownRooms = []
   $scope.publicRooms = []
 
+  // This packs the client for uses on scope functions
+  // .then's must return the client they use for other functions
   var loginPromise = null
-  
+
   $scope.login = function (username, password, homeserver) {
     loginPromise = MatrixManager.login(username, password, homeserver)
     loginPromise.then(function (client) {
       $log.debug('After login: client')
       client.on('sync', function (state, prevState, result) {
         if (state == 'SYNCING') {
-          $scope.$apply(function (scope) {
+          $scope.$apply(function () {
+            $scope.ownRooms = []
             $log.debug('On sync+scope.apply: Result (%s -> %s) %j', prevState, state, result)
-            scope.ownRooms = client.getRooms()
-            $log.debug('Own rooms: %j', scope.ownRooms)
-            scope.isLoggedIn = state != null && state != 'ERROR'
+            $scope.ownRooms = client.getRooms()
+            $log.debug('Own rooms: %j', $scope.ownRooms)
+            $scope.isLoggedIn = true
           })
         }
       })
@@ -36,22 +39,27 @@ var midiOverMatrixCtrl = ['MatrixManager', 'MidiManager', '$scope', '$log', '$in
     if (loginPromise) {
       $log.debug('Stopped client')
       $scope.isLoggedIn = false
-      loginPromise.then(function (client){
+      loginPromise.then(function (client) {
         client.stopClient()
         $log.debug('Stopped client 2')
       })
     }
   }
 
-  $scope.startJamming = function (roomId) {
-    $log.debug('Adding jamming status to %s', roomId)
-    var jamPromise = client.sendStateEvent(roomId, 'm.midi.jamming', {jamming: true}, undefined)
-    jamPromise.then(function success (result) {
-      $log.debug('Result of jamming: %j', result)
-    })
-    jamPromise.catch(function error (result) {
-      $log.debug('Error in jamming: %j', result)
-    })
+  $scope.sendJammingEvent = function (roomId, jamming) {
+    $log.debug('Adding jamming status %s to %s', jamming, roomId)
+
+    if (loginPromise) {
+      loginPromise.then(function (client) {
+        var jamPromise = client.sendStateEvent(roomId, 'm.midi.jamming', {jamming: jamming}, undefined)
+        jamPromise.then(function success (result) {
+          $log.debug('Result of jamming: %j', result)
+        })
+        jamPromise.catch(function error (result) {
+          $log.debug('Error in jamming: %j', result)
+        })
+      })
+    }
   }
 }]
 
